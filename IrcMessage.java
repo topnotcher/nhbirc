c7f2eb0f732fd9def598acd50d781ca8a0836a4e
Greg Bowser <topnotcher@gmail.com>
2011-04-27 03:11:14 -0400

    stuff

--- IrcMessage.java
+++ IrcMessage.java
@@ -2,45 +2,13 @@ import java.util.StringTokenizer;
 import java.util.Vector;
 
 public class IrcMessage {
+
 	private String source = "";
 	private String target = "";
 	private String command = "";
 	private String msg = "";
 
 	private String[] args;
-
-
-/**
- * SEND :NICK mario
- * SEND :USER mario 0 * : mario
- * RECV: :spree.jaundies.com NOTICE AUTH :*** Looking up your hostname...
- * RECV: :spree.jaundies.com NOTICE AUTH :*** Checking ident...
- * RECV: :spree.jaundies.com NOTICE AUTH :*** Couldn't resolve your hostname; using your IP address instead
- * RECV: :spree.jaundies.com NOTICE AUTH :*** No ident response; username sourceed with ~
- * RECV: :spree.jaundies.com 433 * mario :Nickname is already in use.
- * RECV: :spree.jaundies.com 372 fubar :- of the Network will be deemed as irrevocable acceptance of any revisions.
- * :fubar!~fubar@33AFAADF.7A30E887.CC04845D.IP JOIN :#fooooo
- * PING :foo
- *
- */
-/*def parsemsg(s):
-    """Breaks a message from an IRC server into its source, command, and arguments.
-    """
-    source = ''
-    trailing = []
-    if not s:
-       raise IRCBadMessage("Empty line.")
-    if s[0] == ':':
-        source, s = s[1:].split(' ', 1)
-    if s.find(' :') != -1:
-        s, trailing = s.split(' :', 1)
-        args = s.split()
-        args.append(trailing)
-    else:
-        args = s.split()
-    command = args.pop(0)
-    return source, command, args
-*/
 
 	/**
 	 * @TODO this is all messed up.
@@ -57,18 +25,24 @@ public class IrcMessage {
 
 		int pos = -1;
 
+		//check for a leading :, which indicates the meessage has a source
 		if ( msg.charAt(0) == ':' ) {
 			pos = msg.indexOf(' ',1);
 			source = msg.substring(1, pos);
 			msg = msg.substring(pos+1);
 		}
 
+		//check for a " :" delimiter, which separates the message "header" from the body.
 		if ( (pos = msg.indexOf(" :")) != -1 ) {
 			trailing = msg.substring(pos+2);
-			System.out.println("TRAILING " + trailing);
+//			System.out.println("TRAILING " + trailing);
+			
+			//when there is a "header/body",
+			//we only care about parsing 
+			//the header.
 			msg = msg.substring(0,pos); 	
 		}
-
+	
 		for (String arg : msg.split(" "))
 			args.add(arg);
 
@@ -83,18 +57,19 @@ public class IrcMessage {
 
 		this.args = args.toArray(new String[args.size()]);
 
-		for (String arg: this.args)
-			System.out.println("ARG: " + arg);
-
-		System.out.println("MESSAGE: " + this.msg);
-		System.out.println("PREFIX: " + this.source);
-		System.out.println("COMMAND: " + this.command);
+//		for (String arg: this.args)
+//			System.out.println("ARG: " + arg);
+//
+//		System.out.println("MESSAGE: " + this.msg);
+///		System.out.println("PREFIX: " + this.source);
+//		System.out.println("COMMAND: " + this.command);
 
 	}
 
 	public String getCommand() {
 		return command;
 	}
+
 	public String getMessage() {
 		return msg;
 	}
@@ -106,6 +81,7 @@ public class IrcMessage {
 	public String getSource() {
 		return source;
 	}
+
 	public String toString() {
 		return source + " " + command + " " + target + " :"	+msg;
 	}
