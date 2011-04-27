0478b7556385764a8b4a8dec52ced0364be901de
Greg Bowser <topnotcher@gmail.com>
2011-04-27 03:08:44 -0400

    meh

--- IrcMessage.java
+++ IrcMessage.java
@@ -1,4 +1,5 @@
 import java.util.StringTokenizer;
+import java.util.Vector;
 
 public class IrcMessage {
 	private String source = "";
@@ -6,56 +7,89 @@ public class IrcMessage {
 	private String command = "";
 	private String msg = "";
 
+	private String[] args;
+
+
 /**
  * SEND :NICK mario
  * SEND :USER mario 0 * : mario
  * RECV: :spree.jaundies.com NOTICE AUTH :*** Looking up your hostname...
  * RECV: :spree.jaundies.com NOTICE AUTH :*** Checking ident...
  * RECV: :spree.jaundies.com NOTICE AUTH :*** Couldn't resolve your hostname; using your IP address instead
- * RECV: :spree.jaundies.com NOTICE AUTH :*** No ident response; username prefixed with ~
+ * RECV: :spree.jaundies.com NOTICE AUTH :*** No ident response; username sourceed with ~
  * RECV: :spree.jaundies.com 433 * mario :Nickname is already in use.
  * RECV: :spree.jaundies.com 372 fubar :- of the Network will be deemed as irrevocable acceptance of any revisions.
  * :fubar!~fubar@33AFAADF.7A30E887.CC04845D.IP JOIN :#fooooo
  * PING :foo
  *
  */
+/*def parsemsg(s):
+    """Breaks a message from an IRC server into its source, command, and arguments.
+    """
+    source = ''
+    trailing = []
+    if not s:
+       raise IRCBadMessage("Empty line.")
+    if s[0] == ':':
+        source, s = s[1:].split(' ', 1)
+    if s.find(' :') != -1:
+        s, trailing = s.split(' :', 1)
+        args = s.split()
+        args.append(trailing)
+    else:
+        args = s.split()
+    command = args.pop(0)
+    return source, command, args
+*/
 
 	/**
 	 * @TODO this is all messed up.
 	 */
 	public IrcMessage(String msg) {
 
-		int pos = msg.indexOf(':');
-		int start = 0;
 
-		if ( pos == 0 )
-			pos = msg.indexOf(':',1);
+		if (msg.length() == 0)
+			throw new RuntimeException("Empty message");
 
-		//@TODO
-		if ( pos == -1 ) {
-			this.msg = msg;
-			return;
+
+		Vector<String> args = new Vector<String>(2);
+		String trailing = null;
+
+		int pos = -1;
+
+		if ( msg.charAt(0) == ':' ) {
+			pos = msg.indexOf(' ',1);
+			source = msg.substring(1, pos);
+			msg = msg.substring(pos+1);
 		}
 
-		String hdr = msg.substring(0, pos );
-		//fix this..
-		command = hdr.trim();
+		if ( (pos = msg.indexOf(" :")) != -1 ) {
+			trailing = msg.substring(pos+2);
+			System.out.println("TRAILING " + trailing);
+			msg = msg.substring(0,pos); 	
+		}
 
-		this.msg = msg.substring(pos + 1);
+		for (String arg : msg.split(" "))
+			args.add(arg);
+
+		if (trailing != null) {
+			args.add(trailing);
+			this.msg = trailing;
+		} else {
+			this.msg = msg;
+		}
 		
-		StringTokenizer toks = new StringTokenizer(hdr," ");
+		command = args.get(0);
 
-		for (int i = 0; toks.hasMoreTokens() && i < 3; ++i) {
+		this.args = args.toArray(new String[args.size()]);
 
-			if (i == 0) 
-				source = toks.nextToken();
+		for (String arg: this.args)
+			System.out.println("ARG: " + arg);
 
-			else if (i == 1)
-				command = toks.nextToken();
+		System.out.println("MESSAGE: " + this.msg);
+		System.out.println("PREFIX: " + this.source);
+		System.out.println("COMMAND: " + this.command);
 
-			else if (i == 2)
-				target = toks.nextToken();
-		}			  
 	}
 
 	public String getCommand() {
--- Test.java
+++ Test.java
@@ -1,7 +1,22 @@
 class Test {
 	public static void main(String args[]) {
-		IrcMessage msg = new IrcMessage("PING: foo");
 
-		System.out.println (msg.getCommand());
+		String[] msgs = {
+			":spree.jaundies.com NOTICE AUTH :*** No ident response; username prefixed with ~",
+			":spree.jaundies.com 433 * mario :Nickname is already in use.",
+			":spree.jaundies.com 372 fubar :- of the Network will be deemed as irrevocable acceptance of any revisions.",
+			":fubar!~fubar@33AFAADF.7A30E887.CC04845D.IP JOIN :#fooooo",
+			"PING :foo",
+			":trogdor.jaundies.com 353 fubar = #fooooo :@fubar",
+			":trogdor.jaundies.com 366 fubar #fooooo :End of /NAMES list.",
+			":mario!topnotcher@13.37 PRIVMSG #fooooooooooo :This is a test message"
+		};
+
+		for (String raw : msgs)  {
+			System.out.println("");
+			new IrcMessage(raw);
+		}
+
+//		System.out.println (msg.getCommand());
 	}
 }
