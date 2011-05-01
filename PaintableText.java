c00fdd66ae69af8930e8e64c1fab420ca628c865
Greg Bowser <topnotcher@gmail.com>
2011-05-01 15:31:55 -0400

    Comments

--- PaintableText.java
+++ PaintableText.java
@@ -1,12 +1,34 @@
 import java.awt.Graphics;
 	
+/**
+ * Encapsulates a string that can be painted in WrappableTextComponent.
+ */
 interface PaintableText {
 
+	/**
+	 * Paint the whole text on the Graphics context.
+	 *
+	 * Note that this method assumes it has been given its own grahpics context.
+	 * That is: 0,0 should be the TOP LEFT of where this string will be painted.
+	 */
 	public int paint(Graphics g);
 
+	/**
+	 * Paint a substring of this PaintableText
+	 * @see paint(Graphics)
+	 */
 	public int paint(Graphics g, int start, int end);
 
+	/**
+	 * The string stored by this PaintableText
+	 */
 	public String getText();
 
+	/**
+	 * If this next needs to be wrapped,
+	 * lines after the first will be indented by this many spaces.
+	 *
+	 * @return number of spaces to indent
+	 */
 	public int getIndent();
 }
