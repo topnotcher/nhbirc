203bb0d1403a2bf198a79bf49ed6cf2bfe507ace
Greg Bowser <topnotcher@gmail.com>
2011-05-02 17:59:38 -0400

    comments

--- History.java
+++ History.java
@@ -2,6 +2,11 @@ import java.util.ListIterator;
 import java.util.List;
 import util.FixedStack;
 
+/**
+ * Holds a fixed number of commands in the "history buffer"
+ * when adding a command causes the buffer to exceed the maximum size,
+ * the command that has been in the buffer for longest is dropped.
+ */
 public class History {
 
 	/**
@@ -14,25 +19,42 @@ public class History {
 	 */
 	private ListIterator<String> it;
 
+	/**
+	 * Create a new history object with a size of 10 commands
+	 */
 	public History() {
 		//default to ten items.
 		this(10);
 	}
 
+	/**
+	 * @param size Number of commands to store
+	 */
 	public History(int size) {
 		buffer = new FixedStack<String>(size);
 		reset();
 	}
 
+	/**
+	 * Remove all items from history
+	 */
 	public void clear() {
 		buffer.clear();
 		reset();
 	}
 
+	/**
+	 * Resets the "internal pointer" to the first item.
+	 */
 	public void reset() {
 		it = buffer.listIterator();
 	}
 
+	/**
+	 * Add a line to the history.
+	 *
+	 * @param line the command to add to history.
+	 */
 	public void add(String line) {
 
 		//reset();
@@ -47,11 +69,21 @@ public class History {
 		buffer.add(line);
 	}
 	
+	/**
+	 * Move up in history.
+	 * 
+	 * If there are no more items, it returns the last item returned, or "" if there are no items period.
+	 */
 	public String up() {
-		return it.hasNext() ? it.next() : "";
+		return it.hasNext() ? it.next() : (it.hasPrevious() ? it.previous() : "");
 	}
 
+	/**
+	 * Move down in history.
+	 * 
+	 * If there are no more items, it returns the last item returned, or "" if there are no items period.
+	 */
 	public String down() {
-		return it.hasPrevious() ? it.previous() : "";
+		return it.hasPrevious() ? it.previous() : (it.hasNext() ? it.next() : "");
 	}
 }
